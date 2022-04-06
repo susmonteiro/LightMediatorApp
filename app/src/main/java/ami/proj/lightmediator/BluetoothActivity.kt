8202936d98768240c84d24cd50d3dccbe9c2d503
DuBento <duarte.bento@tecnico.ulisboa.pt>
2022-04-06 21:42:58 +0100

    Not working, copied direct code

--- app/src/main/java/ami/proj/lightmediator/BluetoothActivity.kt
+++ app/src/main/java/ami/proj/lightmediator/BluetoothActivity.kt
@@ -1,28 +1,37 @@
 package ami.proj.lightmediator
 
-import ami.proj.lightmediator.databinding.ActivityTranscriptionBinding
-import android.annotation.SuppressLint
-import android.bluetooth.BluetoothAdapter
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.Manifest
+import android.R
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
 import android.companion.AssociationRequest
 import android.companion.BluetoothDeviceFilter
 import android.companion.CompanionDeviceManager
 import android.content.Context
+import android.content.Intent
 import android.content.IntentSender
 import android.content.pm.PackageManager
+import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.util.Log
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentActivity
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
 import java.util.regex.Pattern
 
+private const val TAG = "MY_APP_DEBUG_TAG"
 private const val SELECT_DEVICE_REQUEST_CODE = 0
 
 class BluetoothActivity : AppCompatActivity() {
@@ -32,7 +41,7 @@ class BluetoothActivity : AppCompatActivity() {
             ) { isGranted: Boolean ->
                 if (isGranted) {
                     println("Permission granted!")
-                    enableBluetooth()
+                    connectToDevice()
                 } else {
                     println("Permission not granted...")
                     // TODO send to page
@@ -45,6 +54,19 @@ class BluetoothActivity : AppCompatActivity() {
     }
 
     private val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+
+    /**
+     * The Handler that gets information back from the BluetoothChatService
+     */
+    private val mHandler: Handler = object : Handler() {
+        override fun handleMessage(msg: Message) {
+            println("NEW message:$msg")
+        }
+    }
+
+
+    private val bluetoothService = MyBluetoothService(mHandler)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -74,9 +96,15 @@ class BluetoothActivity : AppCompatActivity() {
             ContextCompat.checkSelfPermission(
                 this, Manifest.permission.BLUETOOTH_CONNECT
             ) -> {
-                enableBluetooth()
+                connectToDevice()
             }
         }
+
+
+    }
+
+    fun connectToDevice() {
+        enableBluetooth()
 
         // To skip filters based on names and supported feature flags (UUIDs),
         // omit calls to setNamePattern() and addServiceUuid()
@@ -143,5 +171,99 @@ class BluetoothActivity : AppCompatActivity() {
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
         }
     }
+
+    @SuppressLint("MissingPermission")
+    private inner class ConnectThread(device: BluetoothDevice) : Thread() {
+
+        private val mmSocket: BluetoothSocket? by lazy(LazyThreadSafetyMode.NONE) {
+            device.createRfcommSocketToServiceRecord("LightUUID")
+        }
+
+        public override fun run() {
+            // Cancel discovery because it otherwise slows down the connection.
+            bluetoothAdapter?.cancelDiscovery()
+
+            mmSocket?.let { socket ->
+                // Connect to the remote device through the socket. This call blocks
+                // until it succeeds or throws an exception.
+                socket.connect()
+
+                // The connection attempt succeeded. Perform work associated with
+                // the connection in a separate thread.
+                val ct = ConnectedThread(socket)
+                ct.write("test".toByteArray())
+            }
+        }
+
+        // Closes the client socket and causes the thread to finish.
+        fun cancel() {
+            try {
+                mmSocket?.close()
+            } catch (e: IOException) {
+                Log.e("BluetoothActivity", "Could not close the client socket", e)
+            }
+        }
+    }
+
+    private inner class ConnectedThread(private val mmSocket: BluetoothSocket) : Thread() {
+
+        private val mmInStream: InputStream = mmSocket.inputStream
+        private val mmOutStream: OutputStream = mmSocket.outputStream
+        private val mmBuffer: ByteArray = ByteArray(1024) // mmBuffer store for the stream
+
+        override fun run() {
+            var numBytes: Int // bytes returned from read()
+
+            // Keep listening to the InputStream until an exception occurs.
+            while (true) {
+                // Read from the InputStream.
+                numBytes = try {
+                    mmInStream.read(mmBuffer)
+                } catch (e: IOException) {
+                    Log.d(TAG, "Input stream was disconnected", e)
+                    break
+                }
+
+                // Send the obtained bytes to the UI activity.
+                val readMsg = mHandler.obtainMessage(
+                    MESSAGE_READ, numBytes, -1,
+                    mmBuffer)
+                readMsg.sendToTarget()
+            }
+        }
+
+        // Call this from the main activity to send data to the remote device.
+        fun write(bytes: ByteArray) {
+            try {
+                mmOutStream.write(bytes)
+            } catch (e: IOException) {
+                Log.e(TAG, "Error occurred when sending data", e)
+
+                // Send a failure message back to the activity.
+                val writeErrorMsg = mHandler.obtainMessage(MESSAGE_TOAST)
+                val bundle = Bundle().apply {
+                    putString("toast", "Couldn't send data to the other device")
+                }
+                writeErrorMsg.data = bundle
+                mHandler.sendMessage(writeErrorMsg)
+                return
+            }
+
+            // Share the sent message with the UI activity.
+            val writtenMsg = mHandler.obtainMessage(
+                MESSAGE_WRITE, -1, -1, mmBuffer)
+            writtenMsg.sendToTarget()
+        }
+
+        // Call this method from the main activity to shut down the connection.
+        fun cancel() {
+            try {
+                mmSocket.close()
+            } catch (e: IOException) {
+                Log.e(TAG, "Could not close the connect socket", e)
+            }
+        }
+    }
+
 
 }
